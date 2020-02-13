08b4aeb3cc711da4f3d90926b485083d26f5e33f
Tremain-Ajena Jones <59344703+Tremain-Ajena@users.noreply.github.com>
2020-02-13 00:43:43 -0500

    first section coded

--- apps.js
+++ apps.js
@@ -1,3 +1,30 @@
-slowMath().then((value) => {
-    return slowMath.add(6,2)
+slowMath.add(1,1).then((num) => {
+    console.log(num);
+    return slowMath.multiply(num, 2);
+    // why is adding a second console.log considered unreachable code
+}).then(num => {
+    console.log(num);
+    return slowMath.divide(num,4);
+}).then(num => {
+    console.log(num);
+    return slowMath.subtract(num, 3);
+}).then(num => {
+    console.log(num);
+    return slowMath.add(num, 98);
+}).then(num => {
+    console.log(num);
+    return slowMath.remainder(num, 2);
+}).then(num => {
+    console.log(num);
+    return slowMath.multiply(num, 50);
+}).then(num => {
+    console.log(num);
+    return slowMath.remainder(num, 40);
+}).then(num => {
+    console.log(num);
+    return slowMath.add(num, 32);
+}).then(num => {
+    console.log(`The final result is ${num}.`)
+}).catch(err => {
+    console.log(err);
 });
